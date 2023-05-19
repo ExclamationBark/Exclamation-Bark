abb8cad80e115f9045ac136d8f4ca2ead63ba86e
ExclamationBark <contact@exclamationbark.com>
2023-05-19 14:44:00 -0500

    Updating

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//////////////// Fix mobile nav ////////////////
 const menu = document.querySelector(".menu");
 const menuItems = document.querySelectorAll(".menuItem");
 const hamburger= document.querySelector(".hamburger");
@@ -20,3 +21,29 @@ function toggleMenu() {
 
 hamburger.addEventListener("click", toggleMenu);
 }
+
+//////////////// Slider ////////////////
+var slidePosition = 1;
+SlideShow(slidePosition);
+
+// Forward/Back controls
+function plusSlides(n) {
+  SlideShow(slidePosition += n);
+}
+
+//  Image controls
+function currentSlide(n) {
+  SlideShow(slidePosition = n);
+}
+
+function SlideShow(n) {
+  var i;
+  var slides = document.getElementsByClassName("slideshow-item");
+  // var circles = document.getElementsByClassName("dots");
+  if (n > slides.length) {slidePosition = 1}
+  if (n < 1) {slidePosition = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  slides[slidePosition-1].style.display = "block";
+} 
